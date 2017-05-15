8db72ccccfe0989696232d9eaf0574723fc176da
Rodrigo Mendez <rodrigo_2392@hotmail.com>
2017-05-15 07:09:12 -0500

    responsive

--- local/resources/views/movil/sidemenu.blade.php
+++ local/resources/views/movil/sidemenu.blade.php
@@ -21,11 +21,11 @@
                   <center><img src="{{url('public/images/logo2.png')}}" alt="" height="100"></center>
             </li>
             <li >
-               <a class="menuitem" href="index.html" onclick="location.href = 'inicio';"><i class="fa fa-home"></i>INICIO</a>
+               <a class="menuitem" href="index.html" onclick="location.href = '{{url('inicio')}}';"><i class="fa fa-home"></i>INICIO</a>
             </li>
-            <li><a class="menuitem" href="faq.html"onclick="location.href = 'perfil';" ><i class="fa fa-user-circle"></i>ESTADO DE CUENTA</a></li>
-            <li><a class="menuitem" href="catalogo.html" onclick="location.href = 'catalogo';"><i class="fa fa-shopping-basket"></i>CATALOGO</a></li>
-            <li><a class="menuitem" href="terminos.html" onclick="location.href = 'terminos';"><i class="fa fa-edit"></i>TERMINOS Y CONDICIONES</a></li>
+            <li><a class="menuitem" href="faq.html"onclick="location.href = '{{url('perfil')}}';" ><i class="fa fa-user-circle"></i>ESTADO DE CUENTA</a></li>
+            <li><a class="menuitem" href="catalogo.html" onclick="location.href = '{{url('catalogo')}}';"><i class="fa fa-shopping-basket"></i>CATALOGO</a></li>
+            <li><a class="menuitem" href="terminos.html" onclick="location.href = '{{url('terminos')}}';"><i class="fa fa-edit"></i>TERMINOS Y CONDICIONES</a></li>
             <li><a class="close-panel menuitem" href="contact.html"><i class="fa fa-sign-out"></i>CERRAR</a></li>
          </ul>
       </div>
